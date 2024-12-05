41e5fc7b663df3639733a1b1ef1ecc49ee060936
Alexandre Rahir <contact@alexandrerahir.be>
2024-12-05 20:13:32 +0100

    Initialisation du projet

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Importation de la bibliothéque discord.js
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// Initialiser les variables d'environnement
+require('dotenv').config();
+
+// Création de l'instance client avec tout les intents
+const client = new Client({ intents: Object.values(GatewayIntentBits) });
+
+// Connexion du client à discord
+client.login(process.env.CLIENT_TOKEN);
